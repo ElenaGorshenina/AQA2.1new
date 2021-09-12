19a3f4519a2799489239661bdaea1a3074afca88
Elena Gorshenina <elenkagorshenina19.07.14@gmail.com>
2021-09-12 23:55:25 +0300

    JSON Schema

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV7.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class MobileBankApiTestV7 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("[0].currency", equalTo("RUB"));
+    }
+}
